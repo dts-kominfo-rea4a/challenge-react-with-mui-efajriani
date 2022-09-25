bc6363107dde62f99ee83fbf77016983810e7c8a
KonanKun <efajriani29@gmail.com>
2022-09-25 09:24:41 +0700

    revisi k2 4th-assignment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import contactsJSON from './data/contacts.json';
 import Contact from './components/Contact';
 import ContactForm from './components/ContactForm';
 import Header from './components/Header';
-import { Grid, Divider } from '@mui/material';
+import { Divider } from '@mui/material';
 
 
 const App = () => {
@@ -26,19 +26,19 @@ const App = () => {
   return (
     <div className="App">
       <Header></Header>
-      <Grid container>
-        <Grid item style={{width:'45%'}}>
+      {/* <Grid container>
+        <Grid item style={{width:'45%'}}> */}
           <ContactForm propsSubmitHandler={(data)=>formSubmitHandler(data)}></ContactForm>
-        </Grid>
-        <Grid item style={{width:'55%', padding:'4em 2em 2em 4em', marginLeft:''}}>
+        {/* </Grid>
+        <Grid item style={{width:'55%', padding:'4em 2em 2em 4em', marginLeft:''}}> */}
           {contactList.map((kontak)=>{return(<>
             <Contact data={kontak}></Contact>
             <Divider></Divider>
           </>
             );
           })}
-        </Grid>
-      </Grid>
+        {/* </Grid>
+      </Grid> */}
       
     </div>
   );
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,7 +2,7 @@
 // https://mui.com/material-ui/react-text-field/#basic-textfield
 // dan Card
 // https://mui.com/material-ui/react-card/#basic-card
-import { Button, Card, TextField } from "@mui/material";
+import { Button, Card, List, ListItem, TextField } from "@mui/material";
 import { useState } from "react";
 import '../App.css';
 
@@ -50,15 +50,27 @@ const ContactForm = (props) => {
             sx={{ bgcolor: '#f3f1eb', padding:'0.5em', margin:'4em 2em',
                 '& .MuiTextField-root': { m: 1, width: '95%' },
             }}>
-                <form 
+                {/* <form 
                 // onSubmit={formOnSubmitHandler}
-                >
-                    <TextField type="text" label="Name" variant="filled" value={inputName} onChange={onChangeName}/>
-                    <TextField type="text" label="Phone" variant="filled" value={inputPhone} onChange={onChangePhone}/>
-                    <TextField type="text" label="Email" variant="filled" value={inputEmail} onChange={onChangeEmail}/>
-                    <TextField type="text" label="Photo URL" variant="filled" value={inputUrl} onChange={onChangeUrl}/>
-                    <Button sx={{color:"green", marginTop:"1em"}} type="submit" onClick={addContactHandler} > ADD NEW</Button>
-                </form>
+                > */}
+                    <List>
+                        <ListItem>
+                            <TextField type="text" label="Name" variant="filled" value={inputName} onChange={onChangeName}/>
+                        </ListItem>
+                        <ListItem>
+                            <TextField type="text" label="Phone" variant="filled" value={inputPhone} onChange={onChangePhone}/>
+                        </ListItem>
+                        <ListItem>
+                            <TextField type="text" label="Email" variant="filled" value={inputEmail} onChange={onChangeEmail}/>
+                        </ListItem>
+                        <ListItem>
+                            <TextField type="text" label="Photo URL" variant="filled" value={inputUrl} onChange={onChangeUrl}/>                           
+                        </ListItem>
+                        <ListItem>
+                            <Button sx={{color:"green", marginTop:"1em"}} type="submit" onClick={addContactHandler} > ADD NEW</Button>
+                        </ListItem>
+                    </List>
+                {/* </form> */}
                 
         </Card>  
          </>
